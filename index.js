70b092760c276f0351bb581308462ba30f89289b
Kris Sakarias <ksakarias@gmail.com>
2018-02-07 15:11:31 -0800

    language selection ids added

--- index.js
+++ index.js
@@ -2,28 +2,33 @@
 
 var sectionEl = document.getElementById('language');
 var divEl = document.getElementById('to-cards');
-
-/* !!!! - function to prevent multiple language selections
-function renderLangs() {
-  var langs = document.querySelectorAll('.lang');
-  console.log(langs);
-}
-*/
+var counter = 0;
 
 sectionEl.addEventListener('click', selectLanguage);
 
 function selectLanguage(e) {
-  var target = e.target;
+  var langs = document.querySelectorAll('.lang');
+
+  for (var i = 0; i < langs.length; i++) {
+    langs[i].id = '';
+  }
+
+  var target = e.srcElement;
+
+  if (target.id !== 'chosen') {
+    target.setAttribute('id', 'chosen');
+  }
+
   localStorage.setItem('language', JSON.stringify(target.name));
-  e.srcElement.setAttribute('class', 'chosen');
 
-  // if ()
-  var nextEl = document.createElement('button');
-  nextEl.textContent = 'To Flashcards';
-  divEl.appendChild(nextEl);
-  divEl.addEventListener('click', function() {
-    window.location.href = 'app.html';
-  });
+  if (counter === 0) {
+    var nextEl = document.createElement('button');
+    nextEl.textContent = 'To Flashcards';
+    divEl.appendChild(nextEl);
+    divEl.addEventListener('click', function() {
+      window.location.href = 'app.html';
+    });
+  }
+
+  counter += 1;
 }
-
-// renderLangs();
