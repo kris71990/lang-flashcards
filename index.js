f6c625342aa0594c30239a5ddd4ed5ab14f03344
Kris Sakarias <ksakarias@gmail.com>
2018-02-07 17:28:44 -0800

    style index with js

--- index.js
+++ index.js
@@ -8,15 +8,28 @@ sectionEl.addEventListener('click', selectLanguage);
 
 function selectLanguage(e) {
   var langs = document.querySelectorAll('.lang');
+  var target = e.srcElement;
+  console.log(e);
 
   for (var i = 0; i < langs.length; i++) {
     langs[i].id = '';
   }
 
-  var target = e.srcElement;
+  target.setAttribute('id', 'chosen');
 
-  if (target.id !== 'chosen') {
-    target.setAttribute('id', 'chosen');
+  if (e.target.name === 'Dutch') {
+    e.target.setAttribute('style', 'background-image: url(img/netherlands.png); background-repeat: no-repeat');
+    // target.style.background = 'url(img/netherlands.png)';
+  } else if (e.target.name === 'German') {
+    e.target.setAttribute('style', 'background-image: url(img/germany.png); background-repeat: no-repeat');
+  } else if (e.target.name === 'French') {
+    e.target.setAttribute('style', 'background-image: url(img/france.png); background-repeat: no-repeat');
+  }
+
+  for (var x = 0; x < langs.length; x++) {
+    if (langs[x].id !== 'chosen') {
+      langs[x].style.background = '';
+    }
   }
 
   localStorage.setItem('language', JSON.stringify(target.name));
