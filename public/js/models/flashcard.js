ee3e7193185543ce83aa3db5079baa297be4c7cc
Kris Sakarias <ksakarias@gmail.com>
2018-04-04 18:25:06 +0200

    change api_url

--- public/js/models/flashcard.js
+++ public/js/models/flashcard.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var app = app || {};
-const __API_URL__ = 'http://localhost:3000';
+// const __API_URL__ = 'http://localhost:3000';
+const __API_URL__ = 'http://language-cards.herokuapp.com';
 
 
 (module => {
