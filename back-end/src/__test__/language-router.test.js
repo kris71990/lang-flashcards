61fc7e258bdd42c2e48d530c7195aa34572a7b00
Kris Sakarias <ksakarias@gmail.com>
2019-03-02 11:52:52 -0800

    test for getting all languages

--- back-end/src/__test__/language-router.test.js
+++ back-end/src/__test__/language-router.test.js
@@ -12,7 +12,7 @@ describe('Language Router Tests', () => {
   afterAll(stopServer);
 
   describe('POST /language', () => {
-    test('Post should return new language (201)', () => {
+    test('POST should return new language (201)', () => {
       return superagent.post(`${API_URL}/language`)
         .send({
           languageName: 'Dutch',
@@ -26,7 +26,7 @@ describe('Language Router Tests', () => {
         });
     });
 
-    test('Post should return conflict (409) if language already exists', () => {
+    test('POST should return conflict (409) if language already exists', () => {
       return mockLanguage('Dutch')
         .then((languageMock) => {
           return superagent.post(`${API_URL}/language`)
@@ -39,10 +39,31 @@ describe('Language Router Tests', () => {
         });
     });
 
-    test('Post should return bad request (400) if missing information', () => {
+    test('POST should return bad request (400) if missing information', () => {
       return superagent.post(`${API_URL}/language`)
         .catch((error) => {
           expect(error.status).toEqual(400);
+        });
+    });
+  });
+
+  describe('GET /languages/all', () => {
+    test('GET should return all languages', () => {
+      return mockLanguage('Dutch')
+        .then((lang1) => {
+          return mockLanguage('German')
+            .then((lang2) => {
+              return superagent.get(`${API_URL}/languages/all`)
+                .then((response) => {
+                  expect(response.status).toEqual(200);
+                  expect(response.body).toBeInstanceOf(Array);
+                  expect(response.body).toHaveLength(2);
+                  expect(response.body[0].languageName).toEqual(lang1.languageName);
+                  expect(response.body[1].languageName).toEqual(lang2.languageName);
+                  expect(response.body[0].languageId).toBeTruthy();
+                  expect(response.body[1].languageId).toBeTruthy();
+                });
+            });
         });
     });
   });
